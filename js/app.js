99c1ddd87e82d2b32d2cf0740d7371313147aa8f
Kim Cordova <cordova.k529@gmail.com>
2017-11-04 14:24:22 -0400

    added html markup

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 		url: 'https://www.reddit.com/top.json',
 		success: function(result){
 			console.log(result.data.children[0].data.title);
-
+      $('#main').html('');
 			for (i = 0; i < result.data.children.length; i++) {
    				var postTitle = result.data.children[i].data.title;
    				var postScore = result.data.children[i].data.score;
@@ -23,9 +23,22 @@ $(document).ready(function(){
 
    				//$('h3').append(postTitle); this doesnt work -- adds all titles to every h3
 
-   				var newTitle = "<h3>" + postTitle + "</h3>"
+          var newArticle = '<article class="article">';
+          newArticle += '<section class="featuredImage">';
+          newArticle += '<img src="' + postThumbnail + '" alt="" />';
+          newArticle += '</section>';
+          newArticle += '<section class="articleContent">';
+          newArticle += '<a href="#"><h3>' + postTitle + '</h3></a>';
+          newArticle += '<h6>Lifestyle</h6>';
+          newArticle += '</section>';
+          newArticle += '<section class="impressions">';
+          newArticle += postScore;
+          newArticle += '</section>';
+          newArticle += '<div class="clearfix"></div>';
+          newArticle += '</article>'
 
-   				$('body').append(newTitle);
+
+   				$('#main').append(newArticle);
 
 
 
